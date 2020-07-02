091e539ae89e16810346e5974b1c255ee74732da
spkgdru <spkgdru@gmail.com>
2020-07-02 22:54:31 +0000

    some fixes

--- src/diffBuilder.js
+++ src/diffBuilder.js
@@ -2,18 +2,37 @@ export default (object1, object2) => {
   const builder = (innerObject1, innerObject2, parentValue) => {
     const keys = Object.keys({ ...innerObject1, ...innerObject2 });
     return keys.reduce((acc, key) => {
-      if ((innerObject1[key] && innerObject2[key]) && ((typeof (innerObject1[key]) === 'object') && (typeof (innerObject2[key]) === 'object'))) {
-        acc[key] = { status: 'object', parent: parentValue, children: builder(innerObject1[key], innerObject2[key]) };
-      } else if (innerObject1[key] && !innerObject2[key]) {
-        acc[key] = { status: 'deleted', parent: parentValue, oldValue: innerObject1[key] };
-      } else if (innerObject2[key] && !innerObject1[key]) {
-        acc[key] = { status: 'added', parent: parentValue, value: innerObject2[key] };
-      } else if ((innerObject1[key] && innerObject2[key]) && (innerObject1[key] !== innerObject2[key])) {
+      if ((typeof (innerObject1[key]) === 'object') && (typeof (innerObject2[key]) === 'object')) {
         acc[key] = {
-          status: 'modified', parent: parentValue, oldValue: innerObject1[key], value: innerObject2[key],
+          status: 'object',
+          parent: parentValue,
+          children: builder(innerObject1[key], innerObject2[key]),
+        };
+      } else if (innerObject1[key] && !innerObject2[key]) {
+        acc[key] = {
+          status: 'deleted',
+          parent: parentValue,
+          oldValue: innerObject1[key],
+        };
+      } else if (innerObject2[key] && !innerObject1[key]) {
+        acc[key] = {
+          status: 'added',
+          parent: parentValue,
+          value: innerObject2[key],
+        };
+      } else if (innerObject1[key] !== innerObject2[key]) {
+        acc[key] = {
+          status: 'modified',
+          parent: parentValue,
+          oldValue: innerObject1[key],
+          value: innerObject2[key],
         };
       } else {
-        acc[key] = { status: 'nonModified', parent: parentValue, value: innerObject2[key] };
+        acc[key] = {
+          status: 'nonModified',
+          parent: parentValue,
+          value: innerObject2[key],
+        };
       }
       return acc;
     }, {});
