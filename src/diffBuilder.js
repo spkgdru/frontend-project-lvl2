5b43784f1a150169e44b063af6872b69de04a076
spkgdru <37667046+spkgdru@users.noreply.github.com>
2020-08-16 20:27:20 +0200

    Update diffBuilder.js

--- src/diffBuilder.js
+++ src/diffBuilder.js
@@ -1,41 +1,25 @@
-export default (parsedConf1, parsedConf2) => {
-  const buildDiff = (config1, config2, parents = []) => {
-    const keys = Object.keys({ ...config1, ...config2 });
-    return keys.reduce((acc, key) => {
-      if ((typeof (config1[key]) === 'object') && (typeof (config2[key]) === 'object')) {
-        acc[key] = {
-          status: 'object',
-          parent: parents,
-          children: buildDiff(config1[key], config2[key], [...parents, key]),
-        };
-      } else if (config1[key] && !config2[key]) {
-        acc[key] = {
-          status: 'deleted',
-          parent: parents,
-          oldValue: config1[key],
-        };
-      } else if (config2[key] && !config1[key]) {
-        acc[key] = {
-          status: 'added',
-          parent: parents,
-          value: config2[key],
-        };
-      } else if (config1[key] !== config2[key]) {
-        acc[key] = {
-          status: 'modified',
-          parent: parents,
-          oldValue: config1[key],
-          value: config2[key],
-        };
-      } else {
-        acc[key] = {
-          status: 'nonModified',
-          parent: parents,
-          value: config2[key],
-        };
-      }
-      return acc;
-    }, {});
-  };
-  return buildDiff(parsedConf1, parsedConf2);
-};
+import _ from 'lodash';
+
+const checkStatus = (previousValue, currentValue) => {
+  if (!previousValue) return "added";
+  if (!currentValue) return "deleted";
+  if (_.isEqual(previousValue, currentValue)) return "unmodified";
+  if (_.isPlainObject(previousValue) && _.isPlainObject(currentValue)) return "isObject";
+  return "changed"; 
+}
+
+const diffBuilder = (object1, object2, path = []) => {
+  const keys = _.uniq([...Object.keys(obj1), ...Object.keys(obj2)]).sort();
+  return keys.reduce((acc, key) => {
+    const previousValue = object1[key];
+    const currentValue = object2[key];
+    const status = checkStatus(previousValue, currentValue);
+    const elem = { key, previousValue, currentValue, status, path };
+    if (status === "isObject") {
+      elem.children = diffBuilder(previousValue, currentValue, [...path, key]);
+    }
+    return [...acc, elem];
+  }, []);
+}
+
+export default builder;
