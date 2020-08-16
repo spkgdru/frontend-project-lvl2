66c02bb7b45543d7af95002be1278ed45e4d5839
spkgdru <37667046+spkgdru@users.noreply.github.com>
2020-08-16 09:17:02 +0200

    Update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,7 +7,8 @@ program
   .version('0.1.0')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format', 'stylish')
+  .action(console.log(genDiff(...program.args, program.format)))
   .arguments('<filepath1> <filepath2>')
   .parse(process.argv);
 
-console.log(genDiff(...program.args, program.format));
+
