eb6de41c19046d14c6ba90f540861e6bfd254969
spkgdru <spkgdru@gmail.com>
2020-08-19 11:24:08 +0000

    some fixes

--- __tests__/parser.test.js
+++ __tests__/parser.test.js
@@ -2,19 +2,35 @@
 
 import fs from 'fs';
 import path from 'path';
+import yaml from 'js-yaml';
+import ini from 'ini';
 import parser from '../src/parsers.js';
 
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
-test('parse JSON 1', () => {
-  const filepath = path.resolve(__dirname, './__fixtures__/json/simpleConfig.json');
+test('parse JSON', () => {
+  const filepath = path.resolve(__dirname, './__fixtures__/json/config1.json');
   const parsedContent = parser(filepath);
   const readedContent = fs.readFileSync(filepath, 'utf-8');
   expect(parsedContent).toEqual(JSON.parse(readedContent));
 });
 
+test('parse YAML', () => {
+  const filepath = path.resolve(__dirname, './__fixtures__/yaml/config1.yaml');
+  const parsedContent = parser(filepath);
+  const readedContent = fs.readFileSync(filepath, 'utf-8');
+  expect(parsedContent).toEqual(yaml.safeLoad(readedContent));
+});
+
+test('parse INI', () => {
+  const filepath = path.resolve(__dirname, './__fixtures__/ini/config1.ini');
+  const parsedContent = parser(filepath);
+  const readedContent = fs.readFileSync(filepath, 'utf-8');
+  expect(parsedContent).toEqual(ini.decode(readedContent));
+});
+
 test('parse unsupported file', () => {
-  const filepath = path.resolve(__dirname, './__fixtures__/json/simpleConfig.scr');
+  const filepath = path.resolve(__dirname, './__fixtures__/json/config1.scr');
   expect(() => {
     parser(filepath);
   }).toThrow();
