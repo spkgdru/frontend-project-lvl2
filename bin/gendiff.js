99ff088de3571efca900efdec1bda2ae6fbd9295
spkgdru <37667046+spkgdru@users.noreply.github.com>
2020-09-29 03:54:16 +0200

    Update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,8 +4,8 @@ import program from 'commander';
 import genDiff from '../index.js';
 
 program
-  .version('0.1.0')
-  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .description('Compares two configuration files and shows a difference')
   .option('-f, --format [type]', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2, program.format)))
