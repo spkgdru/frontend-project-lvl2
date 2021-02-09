7e51b123ef1d8c96eca1b3753e9cb6742e27bd21
spkgdru <spkgdru@gmail.com>
2021-02-09 20:46:16 +0000

    update tests

--- __tests__/gendiff2.test.js
+++ __tests__/gendiff2.test.js
@@ -5,23 +5,11 @@ import path from 'path';
 import gendiff from '../index.js';
 
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
-const filepath1 = path.resolve(__dirname, './__fixtures__/yaml/file1.yaml');
-const filepath2 = path.resolve(__dirname, './__fixtures__/yaml/file2.yaml');
+const filepath1 = path.resolve(__dirname, './__fixtures__/json/config1.json');
+const filepath2 = path.resolve(__dirname, './__fixtures__/json/config2.json');
 
 test('gendiffInStylish', () => {
-  const diffFilepath = path.resolve(__dirname, './__fixtures__/results/diff1');
+  const diffFilepath = path.resolve(__dirname, './__fixtures__/results/diff4');
   const result = fs.readFileSync(diffFilepath, 'utf-8');
   expect(gendiff(filepath1, filepath2)).toBe(result.trim());
-});
-
-test('gendiffInPlain', () => {
-  const diffFilepath = path.resolve(__dirname, './__fixtures__/results/diff2');
-  const result = fs.readFileSync(diffFilepath, 'utf-8');
-  expect(gendiff(filepath1, filepath2, 'plain')).toBe(result.trim());
-});
-
-test('gendiffInJSON', () => {
-  const diffFilepath = path.resolve(__dirname, './__fixtures__/results/diff3');
-  const result = fs.readFileSync(diffFilepath, 'utf-8');
-  expect(gendiff(filepath1, filepath2, 'json')).toBe(result.trim());
 });
--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -4,45 +4,45 @@ const indentSymbol = ' ';
 const spacesCount = 4;
 
 const printValue = (value, indentLevel) => {
-    if ((!_.isPlainObject(value)) || (value === null)) return value;
-    const screenLines = Object.entries(value).map(([paramName, paramValue]) => {
-        const outputIndent = indentSymbol.repeat(spacesCount * indentLevel);
-        const outputValue = paramName + `: ` + printValue(paramValue, indentLevel + 1);
-        return outputIndent + outputValue;
-    });
-    const closingBracketLine = indentSymbol.repeat(spacesCount * (indentLevel - 1)) + '}'; 
-    return ['{', ...screenLines, closingBracketLine].join('\n');
-}
+  if ((!_.isPlainObject(value)) || (value === null)) return value;
+  const screenLines = Object.entries(value).map(([paramName, paramValue]) => {
+    const outputIndent = indentSymbol.repeat(spacesCount * indentLevel);
+    const outputValue = `${paramName}: ${printValue(paramValue, indentLevel + 1)}`;
+    return outputIndent + outputValue;
+  });
+  const closingBracketLine = `${indentSymbol.repeat(spacesCount * (indentLevel - 1))}}`;
+  return ['{', ...screenLines, closingBracketLine].join('\n');
+};
 
 export default (diff) => {
-    const iter = (sourceDiff, depth = 1) => {
-        const indentLine = indentSymbol.repeat(spacesCount * depth - 2);
-        const closingBracket = indentSymbol.repeat(spacesCount * (depth - 1)) + '}';
-        const lines = sourceDiff.reduce((acc, value) => {
-            if (value.status === 'nested') {
-                const output = `  ${value.key}: ${iter(value.children, depth + 1)}`; 
-                return [...acc, indentLine + output];
-            } 
-            else if (value.status === 'added') {
-                const output = `+ ${value.key}: ${printValue(value.currentValue, depth + 1)}`;
-                return [...acc, indentLine + output];
-            }
-            else if (value.status === 'deleted') {
-                const output = `- ${value.key}: ${printValue(value.previousValue, depth + 1)}`;
-                return [...acc, indentLine + output];
-            }
-            else if (value.status === 'unmodified') {
-                const output = `  ${value.key}: ${printValue(value.currentValue, depth + 1)}`;
-                return [...acc, indentLine + output];
-            }
-            else if (value.status === 'changed') {
-                const output1 = `- ${value.key}: ${printValue(value.previousValue, depth + 1)}`;
-                const output2 = `+ ${value.key}: ${printValue(value.currentValue, depth + 1)}`;
-                return [...acc, indentLine + output1, indentLine + output2];
-            }
-            return acc;  
-        }, []);
-        return ['{', ...lines, closingBracket].join('\n');
-    }
-    return iter(diff);
-}
+  const iter = (sourceDiff, depth = 1) => {
+    const indentLine = indentSymbol.repeat(spacesCount * depth - 2);
+    const closingBracket = `${indentSymbol.repeat(spacesCount * (depth - 1))}}`;
+    const lines = sourceDiff.reduce((acc, value) => {
+      if (value.status === 'nested') {
+        const output = `  ${value.key}: ${iter(value.children, depth + 1)}`;
+        return [...acc, indentLine + output];
+      }
+      if (value.status === 'added') {
+        const output = `+ ${value.key}: ${printValue(value.currentValue, depth + 1)}`;
+        return [...acc, indentLine + output];
+      }
+      if (value.status === 'deleted') {
+        const output = `- ${value.key}: ${printValue(value.previousValue, depth + 1)}`;
+        return [...acc, indentLine + output];
+      }
+      if (value.status === 'unmodified') {
+        const output = `  ${value.key}: ${printValue(value.currentValue, depth + 1)}`;
+        return [...acc, indentLine + output];
+      }
+      if (value.status === 'changed') {
+        const output1 = `- ${value.key}: ${printValue(value.previousValue, depth + 1)}`;
+        const output2 = `+ ${value.key}: ${printValue(value.currentValue, depth + 1)}`;
+        return [...acc, indentLine + output1, indentLine + output2];
+      }
+      return acc;
+    }, []);
+    return ['{', ...lines, closingBracket].join('\n');
+  };
+  return iter(diff);
+};
